e180134906d03670e4ffb54e2261298a3bb0ad35
Coto <programmer.coto.1988@gmail.com>
2020-04-28 03:33:44 -0400

    Update Binary.
    TGDS Changes:
    - cleanup TGDS Fifo structs

--- common/ipcfifoTGDSUser.h
+++ common/ipcfifoTGDSUser.h
@@ -22,11 +22,11 @@ USA
 //TGDS required version: IPC Version: 1.3
 
 //IPC FIFO Description: 
-//		struct sIPCSharedTGDS * TGDSIPC = TGDSIPCStartAddress; 														// Access to TGDS internal IPC FIFO structure. 		(ipcfifoTGDS.h)
-//		struct sIPCSharedTGDSSpecific * TGDSUSERIPC = (struct sIPCSharedTGDSSpecific *)TGDSIPCUserStartAddress;		// Access to TGDS Project (User) IPC FIFO structure	(ipcfifoTGDSUser.h)
+//		TGDSIPC 		= 	Access to TGDS internal IPC FIFO structure. 		(ipcfifoTGDS.h)
+//		TGDSUSERIPC		=	Access to TGDS Project (User) IPC FIFO structure	(ipcfifoTGDSUser.h)
 
-#ifndef __specific_shared_h__
-#define __specific_shared_h__
+#ifndef __ipcfifoTGDSUser_h__
+#define __ipcfifoTGDSUser_h__
 
 #include "dsregs.h"
 #include "dsregs_asm.h"
@@ -53,11 +53,13 @@ struct gbaheader_t{
 	u16 res;
 };
 
-struct sIPCSharedTGDSSpecific{
+typedef struct sIPCSharedTGDSSpecific{
 	uint32 * IPC_ADDR;
     uint8 * ROM;   		//pointer to ROM page
     int rom_size;   	//rom total size
-};
+}  IPCSharedTGDSSpecific	__attribute__((aligned (4)));
+
+#define TGDSUSERIPC ((IPCSharedTGDSSpecific volatile *)(0x027FF000 + TGDSIPCSize))
 
 //Settings:
 
