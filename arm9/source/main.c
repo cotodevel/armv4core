ae252d9fdc69138915412dbafecbed51320b0d57
Coto <programmer.coto.1988@gmail.com>
2022-09-02 19:02:39 -0400

    ARM9 newlib uses hardware divider unit and square root unit, which speeds up 3D and multimedia tasks

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -143,6 +143,7 @@ int main(int argc, char **argv) {
 	RenderTGDSLogoMainEngine((uint8*)&TGDSLogoLZSSCompressed[0], TGDSLogoLZSSCompressed_size);
 	
 	/////////////////////////////////////////////////////////Reload TGDS Proj///////////////////////////////////////////////////////////
+	#if !defined(ISEMULATOR)
 	char tmpName[256];
 	char ext[256];	
 	char TGDSProj[256];
@@ -220,6 +221,7 @@ int main(int argc, char **argv) {
 			
 		}
 	}
+	#endif
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	if(__dsimode == true){
 		TWLSetTouchscreenTWLMode();
