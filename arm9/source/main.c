d74fa2d06e3bfa8edbdc7cdcd90b1112e3fc4725
cotodevel <programmer.coto.1988@gmail.com>
2026-05-11 09:50:49 -0400

    TGDS 2026:  -TGDS-MB v3 is fully integrated on TGDS + WoopsiSDK project.
    Stable on NTR/TWL hardware and guaranteed to work on such hardware (DSiXL as well a DS Phat).
    
    -Fixed TGDS NTR/TWL compatibility in popular loaders.
    
    -
    
    TGDS 2025:
    -Jan 31, 2025: Implement proper ARM7(i) and ARM9(i) IntrWait/VBlankIntrWait bios calls replacement.
    Now TWL mode works correctly on stuff like TGDSProject3D
    
    -
    
    Update int runThreads(struct task_Context * taskCtx, bool waitForVblk), now CPU sleeps until any interrupt, or a vblank interrupt.
    
    -
    
    -Add getTGDSThreadSystem(), pauseThread(), resumeThread(), removeThread().
    -ARM7 as well ARM9 uses TGDS threading now.
    -Moved ARM7 touchscreen code as task.
    -Threading is initialized by default, TGDS Projects will just require to inject tasks to run them.
    
    -
    
    -wait for irq when tasks are done, to prevent artificial locksteps between threads.
    
    Note: This commit removes unnecessary stuttering on TGDS-multimediaplayer.
    
    -
    
    -Threads are ran independently over milliseconds or microseconds (highest priority).
    
    This enables a TGDS Project to save timers, keep CPU usage low, being highly performant as well run threads on a timely basis.
    
    -
    
    Update TGDS Threading:
    
    tUnitsMilliseconds(Milliseconds) are queued and counted up to NDS timers. Assigned to less resource intensive tasks. (CPU + Timer interrupts)
    tUnitsMicroseconds(Microseconds) are given the highest thread priority. Assigned to most resource intensive tasks (CPU interrupts)
    
    -
    
    -increase threading granularity, so we can count between milliseconds or microseconds, to give tasks more CPU cycles
    
    -
    
    Restore GDB Stub support.
    If an Exception occurs, and TGDS Project has WIFI available for ARM7 & ARM9 cores, GDB debugging will be enabled. Otherwise, it's disabled.
    
    -
    
    TGDS Project now uses threading for process:
    
    -handleTurnOnTurnOffScreenTimeout();
    
    -
    
    -add -fno-short-enums, fixes erratic code behaviour on TGDS Filesystem
    
    -
    
    8 June 2025: Migrate TGDS ecosystem from Bitbucket into Github

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -71,31 +71,34 @@ USA
 #include "global_settings.h"
 #include "TGDS_threads.h"
 
+//TGDS Project's ARM7 VRAM Core ARM7 @ 0x06000000
+#include "arm7vram.h"
+#include "arm7vram_twl.h"
+
+u32 * getTGDSARM7VRAMCore(){
+	if(__dsimode == false){
+		swiDecompressLZSSWram((u8*)&arm7vram[0], (u8*)TGDS_MB_V3_ARM7_SCRATCHPAD_LZSS_DECOMP_BUF);
+	}
+	else{
+		swiDecompressLZSSWram((u8*)&arm7vram_twl[0], (u8*)TGDS_MB_V3_ARM7_SCRATCHPAD_LZSS_DECOMP_BUF);
+	}
+	return (u32*)TGDS_MB_V3_ARM7_SCRATCHPAD_LZSS_DECOMP_BUF;
+}
+
 //TGDS-MB ARM7 Bootldr
 #include "arm7bootldr.h"
 #include "arm7bootldr_twl.h"
 
-//TGDS-MB ARM7 Stage 1
-#include "arm7_stage1.h"
-#include "arm7_stage1_twl.h"
-
 u32 * getTGDSMBV3ARM7Bootloader(){	//Required by ToolchainGenericDS-multiboot v3
 	if(__dsimode == false){
-		return (u32*)&arm7bootldr[0];	
+		swiDecompressLZSSWram((u8*)&arm7bootldr[0], (u8*)TGDS_MB_V3_ARM7_SCRATCHPAD_LZSS_DECOMP_BUF);	
 	}
 	else{
-		return (u32*)&arm7bootldr_twl[0];
+		swiDecompressLZSSWram((u8*)&arm7bootldr_twl[0], (u8*)TGDS_MB_V3_ARM7_SCRATCHPAD_LZSS_DECOMP_BUF);
 	}
+	return (u32*)TGDS_MB_V3_ARM7_SCRATCHPAD_LZSS_DECOMP_BUF;
 }
 
-u32 * getTGDSMBV3ARM7Stage1(){	//required by TGDS-mb v3's ARM7 @ 0x03800000
-	if(__dsimode == false){
-		return (u32*)&arm7_stage1[0];	
-	}
-	else{
-		return (u32*)&arm7_stage1_twl[0];
-	}
-}
 
 char curChosenBrowseFile[MAX_TGDSFILENAME_LENGTH];
 char biospath[MAX_TGDSFILENAME_LENGTH+1];
@@ -168,9 +171,14 @@ int main(int argc, char **argv)
 
 	#ifdef ARM9
 	/*			TGDS 1.6 Standard ARM9 Init code start	*/
+	
 	//Save Stage 1: IWRAM ARM7 payload: NTR/TWL (0x03800000)
-	memcpy((void *)TGDS_MB_V3_ARM7_STAGE1_ADDR, (const void *)getTGDSMBV3ARM7Stage1(), (int)(96*1024));
-	coherent_user_range_by_size((uint32)TGDS_MB_V3_ARM7_STAGE1_ADDR, (int)(96*1024));
+	memcpy((void *)TGDS_MB_V3_ARM7_STAGE1_ADDR, (const void *)0x02380000, (int)(96*1024));	//
+	coherent_user_range_by_size((uint32)TGDS_MB_V3_ARM7_STAGE1_ADDR, (int)(96*1024)); //		also for TWL binaries 
+	
+	//Execute Stage 2: VRAM ARM7 payload: NTR/TWL (0x06000000)
+	u32 * payload = getTGDSARM7VRAMCore();
+	executeARM7Payload((u32)0x02380000, 96*1024, payload);
 	
 	//NTR mode requires ARM7DLDI layout set up before malloc setup
 	if(__dsimode == false){
@@ -210,9 +218,8 @@ int main(int argc, char **argv)
 	if(__dsimode == true){
 		TWLSetTouchscreenTWLMode();
 	}
-	REG_IME = 1;
-	
 	setupDisabledExceptionHandler();
+	REG_IME = 1;
 	
 	// GBA EMU INIT
 	biospath[0] = 0;
@@ -231,6 +238,11 @@ int main(int argc, char **argv)
 	//Boot .NDS file! (homebrew only)
 	bool isTGDSTWLHomebrew = false;
 	if(isNTROrTWLBinary(curChosenBrowseFile, &isTGDSTWLHomebrew) != notTWLOrNTRBinary){
+		
+		//Execute Stage 1: IWRAM ARM7 payload: NTR/TWL (0x03800000)
+		payload = (u32 *)TGDS_MB_V3_ARM7_STAGE1_ADDR;
+		executeARM7Payload((u32)0x02380000, 96*1024, payload);
+
 		char thisArgv[3][MAX_TGDSFILENAME_LENGTH];
 		memset(thisArgv, 0, sizeof(thisArgv));
 		strcpy(&thisArgv[0][0], "");	//Arg0:	This Binary loaded
